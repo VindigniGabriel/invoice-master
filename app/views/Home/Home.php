2932a3ad17ff97f3bd91738e9daeea1c8bcfe341
VindigniGabriel <vindignigb@gmail.com>
2018-05-11 09:34:08 -0400

    first commit for PDF Invoice WEB

---
+++ app/views/Home/Home.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') or exit('No se permite acceso directo'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+</head>
+<body>
+ <h1>Prueba</h1>
+</body>
+</html>
---
+++ app/views/Login/login.php
@@ -0,0 +1,64 @@
+<?php defined('BASEPATH') or exit('No se permite acceso directo'); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+  
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #eee;
+      }
+
+      .form-signin {
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin .checkbox {
+        font-weight: normal;
+      }
+      .form-signin .form-control {
+        position: relative;
+        height: auto;
+        -webkit-box-sizing: border-box;
+           -moz-box-sizing: border-box;
+                box-sizing: border-box;
+        padding: 10px;
+        font-size: 16px;
+      }
+      .form-signin .form-control:focus {
+        z-index: 2;
+      }
+      .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+      .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
+    </style>
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+   Login
+
+    </div> 
+
+  </body>
+</html>
